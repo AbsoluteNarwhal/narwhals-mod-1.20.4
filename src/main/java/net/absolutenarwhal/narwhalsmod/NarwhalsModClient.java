b889b60bc90c5ba644d56bf02758b9b6c3b8fc11
Alexander Martin <absolutenarwhal@gmail.com>
2024-03-11 19:33:26 +0000

    Initial commit

---
+++ src/main/java/net/absolutenarwhal/narwhalsmod/NarwhalsModClient.java
@@ -0,0 +1,10 @@
+package net.absolutenarwhal.narwhalsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NarwhalsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
